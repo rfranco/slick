cd0bb7979e8f1cb1958330cf4ee255a5bcd456f3
Christopher Vogt <christopher.vogt@epfl.ch>
2013-12-10 04:12:14 +0100

    Improve scaladoc output (src links, start page, pkg docs)

---
+++ src/main/scala/scala/slick/jdbc/meta/package.scala
@@ -0,0 +1,3 @@
+package scala.slick.jdbc
+/** A wrapper around jdbc's meta data api and logic to create a Slick model from it. */
+package object meta
---
+++ src/main/scala/scala/slick/lifted/package.scala
@@ -0,0 +1,3 @@
+package scala.slick
+/** Lifted embedding: Stable query api based on implicits and overloading lifting Scala code into Slick AST's */
+package object lifted
